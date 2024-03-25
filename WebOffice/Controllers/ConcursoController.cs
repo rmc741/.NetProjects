ae85302cba3013da5cce024f07f4b699ed2d0ace
RafaelMendonca <rafael.medonca2@gmail.com>
2024-03-24 23:30:36 -0300

    commit inicial.

---
+++ WebOffice/Controllers/ConcursoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebOffice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConcursoController : ControllerBase
+    {
+    }
+}
